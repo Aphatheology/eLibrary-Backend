73efe6154ba713f73650c20f30283095de666784
Aphatheology <mustapha.abdulkareem@saritay.com>
2024-01-01 11:41:14 +0100

    getting started

---
+++ src/main/java/com/aphatheology/elibrarybackend/ElibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aphatheology.elibrarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElibraryBackendApplication.class, args);
+    }
+
+}
